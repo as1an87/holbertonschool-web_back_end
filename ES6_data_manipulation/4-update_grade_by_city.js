eeb77b047f2890bc28bea693bb0910c1eb12c27b
as1an87 <as1anoff87@gmail.com>
2024-07-23 10:47:42 +0000

    Combine

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,12 +1,10 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-  return students
-    .filter(student => student.location === city)
-    .map(student => {
-      const gradeEntry = newGrades.find(grade => grade.studentId === student.id);
+  return students.filter(({ location }) => location === city)
+    .map((student) => {
+      const grade = newGrades.filter(({ studentId }) => studentId === student.id)[0];
       return {
         ...student,
-        grade: gradeEntry ? gradeEntry.grade : 'N/A',
+        grade: grade ? grade.grade : 'N/A',
       };
     });
 }
-
