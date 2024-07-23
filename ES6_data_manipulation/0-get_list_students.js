51b2288214cc399c713b9d9c6b10da325fbfe934
as1an87 <as1anoff87@gmail.com>
2024-07-23 09:06:56 +0000

    Basic list of objects

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,8 @@
+function getListStudents() {
+	return [
+		{id: 1, firstname: 'Guillaume', location: 'San Franscisco' },
+		{ id: 2, firstName: 'James', location: 'Columbia' },
+    		{ id: 5, firstName: 'Serena', location: 'San Francisco' }
+	];
+}
+export default getListStudents;
