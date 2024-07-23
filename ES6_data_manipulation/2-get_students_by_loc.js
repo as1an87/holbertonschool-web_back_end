45770865b5a0976f26e8421ba88f0b60cb422004
as1an87 <as1anoff87@gmail.com>
2024-07-23 10:45:01 +0000

    Filter

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,3 @@
-export default function getStudentsByLocation(students, city) {
- return students.filter(({student}) => student.location === city);
+export default function getStudentsByLocation(students, place) {
+  return students.filter(({ location }) => location === place);
 }
