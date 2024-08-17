f64adea5b19a3043cc9e7463f18fdc7771cc5bea
as1an87 <as1anoff87@gmail.com>
2024-08-17 20:15:59 +0000

    Basic annotations - concat

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-"""a concatenated string"""
+"""Concat strings"""
 
 
 def concat(str1: str, str2: str) -> str:
-   """ a concatenated string"""
+    """concat strings"""
     return str1 + str2
