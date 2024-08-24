714306f907fde892af8dde67321b9faeb7463030
as1an87 <as1anoff87@gmail.com>
2024-08-24 20:00:18 +0000

    Async Comprehensions

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
-"""Function"""
+"""
+Function
+"""
 import asyncio
-from 0_async_generator import async_generator
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
-    """Return"""
+async def async_comprehension() -> List[float]:
+    """
+    Return
+    """
     return [i async for i in async_generator()]
