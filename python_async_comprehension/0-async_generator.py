2c144552e764aaf7771e5b68bc4d6061b0479ebc
as1an87 <as1anoff87@gmail.com>
2024-08-24 20:07:57 +0000

    Async Generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
-"""Function"""
+"""
+Function
+"""
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
-    """Return"""
-    for _ in range(10):
+async def async_generator() -> Generator[float, None, None]:
+    """
+    Return
+    """
+    for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
