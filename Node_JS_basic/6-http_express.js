a49acd2515b59b05e6ddd2bf498028e168f38d56
as1an87 <as1anoff87@gmail.com>
2024-09-22 17:49:04 +0000

    Create a small HTTP server using Express

---
+++ Node_JS_basic/6-http_express.js
@@ -0,0 +1,15 @@
+
+/* eslint-disable */
+
+const express = require('express');
+
+const port = 1245;
+const app = express();
+module.exports = app;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.listen(port);
+
